28f52feba97e10f21a06942f6a02921e6b1294f9
baalzx <191440199@qq.com>
2017-02-27 22:58:41 +0800

    读取数据库前端代码

---
+++ DatabaseFramework/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DatabaseFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.dgv_Mc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_Lx = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_Yxk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_Ms = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_Zj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv_Mc,
+            this.dgv_Lx,
+            this.dgv_Yxk,
+            this.dgv_Ms,
+            this.dgv_Zj});
+            this.dgv.Location = new System.Drawing.Point(89, 186);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 23;
+            this.dgv.Size = new System.Drawing.Size(240, 150);
+            this.dgv.TabIndex = 2;
+            // 
+            // dgv_Mc
+            // 
+            this.dgv_Mc.HeaderText = "Column1";
+            this.dgv_Mc.Name = "dgv_Mc";
+            // 
+            // dgv_Lx
+            // 
+            this.dgv_Lx.HeaderText = "Column2";
+            this.dgv_Lx.Name = "dgv_Lx";
+            // 
+            // dgv_Yxk
+            // 
+            this.dgv_Yxk.HeaderText = "Column3";
+            this.dgv_Yxk.Name = "dgv_Yxk";
+            // 
+            // dgv_Ms
+            // 
+            this.dgv_Ms.HeaderText = "Column4";
+            this.dgv_Ms.Name = "dgv_Ms";
+            // 
+            // dgv_Zj
+            // 
+            this.dgv_Zj.HeaderText = "dgv_Zj";
+            this.dgv_Zj.Name = "dgv_Zj";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 366);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_Mc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_Lx;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_Yxk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_Ms;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_Zj;
+    }
+}
+
---
+++ DatabaseFramework/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using com.tjigs.DbAccessFramework;
+using System.Data.OleDb;
+
+namespace DatabaseFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // DbUtility DbAccess=new DbUtility ()
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            openFileDialog1.ShowDialog();
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            DbUtility AccessDB = new DbUtility(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + openFileDialog1.FileName,DbProviderType .Access );
+            List<string>tablenames=AccessDB.GetTableName();
+          //  textBox1.Text = openFileDialog1.FileName;
+            dgv.Rows.Clear();
+            foreach (string name in tablenames)
+            {
+               DataTable table = AccessDB.GetDbField(name);
+
+
+
+                //int i = 0;
+
+                //foreach (DataRow row in table.Rows)
+                //{
+
+                //    dgv.Rows.Add(1);
+                //    dgv.Rows[i].Cells["dgv_Mc"].Value = row["COLUMN_NAME"].ToString();    //字段名称
+
+                //    int lx = int.Parse(row["DATA_TYPE"].ToString());    //字段类型
+
+                //    switch (lx)
+                //    {
+                //        case 2: dgv.Rows[i].Cells["dgv_Lx"].Value = "int"; break;
+                //        case 3: dgv.Rows[i].Cells["dgv_Lx"].Value = "int"; break;
+                //        case 4: dgv.Rows[i].Cells["dgv_Lx"].Value = "Single"; break;
+                //        case 5: dgv.Rows[i].Cells["dgv_Lx"].Value = "double"; break;
+                //        case 6: dgv.Rows[i].Cells["dgv_Lx"].Value = "decimal"; break;
+                //        case 7: dgv.Rows[i].Cells["dgv_Lx"].Value = "DateTime"; break;
+                //        case 11: dgv.Rows[i].Cells["dgv_Lx"].Value = "bool"; break;
+                //        case 17: dgv.Rows[i].Cells["dgv_Lx"].Value = "byte"; break;
+                //        case 72: dgv.Rows[i].Cells["dgv_Lx"].Value = "string"; break;
+                //        case 130: dgv.Rows[i].Cells["dgv_Lx"].Value = "string"; break;
+                //        case 131: dgv.Rows[i].Cells["dgv_Lx"].Value = "decimal"; break;
+                //        case 128: dgv.Rows[i].Cells["dgv_Lx"].Value = "string"; break;
+                //        default: dgv.Rows[i].Cells["dgv_Lx"].Value = "string"; break;
+                //    }
+                //    dgv.Rows[i].Cells["dgv_Lx"].Tag = dgv.Rows[i].Cells["dgv_Lx"].Value.ToString();
+                //    dgv.Rows[i].Cells["dgv_Yxk"].Value = bool.Parse(row["IS_NULLABLE"].ToString());    //允许空
+                //    dgv.Rows[i].Cells["dgv_Ms"].Value = row["DESCRIPTION"].ToString();    //字段说明、描述
+                //    dgv.Rows[i].Cells["dgv_Zj"].Value = false;
+
+                //    i++;
+                //}
+             //  DataTable dt= AccessDB.GetDbPK(name);
+               dgv.DataSource = table;
+               //foreach (DataRow dr in dt.Rows )
+               //{
+               //    for (int j = 0; j < dgv.RowCount-1; j++)
+               //    {
+               //        if (dgv.Rows[j].Cells["dgv_mc"].Value.ToString() == dr["COLUMN_NAME"].ToString())
+               //        {
+               //            dgv.Rows[j].Cells["dgv_Zj"].Value = true;    //主键
+               //        }
+               //    }
+               //}
+                   
+            }
+          
+
+                    //table = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Primary_Keys, null);    //主键
+
+                    //foreach (DataRow row in table.Rows)
+                    //{
+                    //    if (sjbmc == row["TABLE_NAME"].ToString())
+                    //    {
+                    //        for (int j = 0; j < dgv.RowCount; j++)
+                    //        {
+                    //            if (dgv.Rows[j].Cells["dgv_mc"].Value.ToString() == row["COLUMN_NAME"].ToString())
+                    //            {
+                    //                dgv.Rows[j].Cells["dgv_Zj"].Value = true;    //主键
+                    //            }
+                    //        }
+                    //    }
+                    //}
+
+                  //  connection.Close();
+           // DbUtility DbAccess = new DbUtility(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + openFileDialog1.FileName + ";Persist Security Info=True", DbProviderType.Access);
+           // DbAccess.
+        }
+        
+    }
+}
